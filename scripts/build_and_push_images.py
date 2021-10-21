8511b9a7e9ebbc6d57c00c576052f20baad5d5dd
migraf <michael.graf3110@gmail.com>
2021-10-21 20:15:05 +0200

    changed dir requirements for auto builds

--- scripts/build_and_push_images.py
+++ scripts/build_and_push_images.py
@@ -2,6 +2,7 @@ import docker
 import os
 import typing
 import os.path
+from dotenv import load_dotenv, find_dotenv
 
 
 def build_images():
@@ -17,20 +18,22 @@ def build_images():
 
 def process_docker_image_dir(directory, client, registry):
     sub_directories = scandir(directory)
+
     for sub_dir in sub_directories:
+        print(sub_dir)
         repository_parts = sub_dir.split(os.sep)[1:]
-        if len(repository_parts) >= 2:
+        if len(repository_parts) >= 3:
             repository_name = "/".join(repository_parts)
             repository_name = registry + "/master/" + repository_name
             build_path = os.path.abspath(sub_dir)
 
             print(f"Building image <{repository_name}> at path <{build_path}>")
-            image, logs = client.images.build(path=build_path, tag=repository_name + ":latest")
-            for item in logs:
-                print(item)
-            # push the image to the registry
-            for line in client.api.push(repository=repository_name, tag="latest", stream=True, decode=True):
-                print(line)
+            # image, logs = client.images.build(path=build_path, tag=repository_name + ":latest")
+            # for item in logs:
+            #     print(item)
+            # # push the image to the registry
+            # for line in client.api.push(repository=repository_name, tag="latest", stream=True, decode=True):
+            #     print(line)
 
 
 def scandir(start_dir) -> typing.List[typing.Tuple[str, str]]:
