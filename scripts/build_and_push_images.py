019b6ebf7648edb629c334c5af9caa47a639e709
migraf <michael.graf3110@gmail.com>
2021-10-22 09:42:15 +0200

    updated build script

--- scripts/build_and_push_images.py
+++ scripts/build_and_push_images.py
@@ -8,10 +8,6 @@ from dotenv import load_dotenv, find_dotenv
 def build_images():
     client = docker.from_env()
     registry = os.getenv("CONTAINER_REGISTRY")
-    # username = os.getenv("REGISTRY_USER")
-    # password = os.getenv("REGISTRY_PASSWORD")
-    # login_result = client.login(registry=registry, username=username, password=password)
-    # print(f"Login result: {login_result}")
     process_docker_image_dir("./language", client, registry)
     process_docker_image_dir("./custom", client, registry)
 
@@ -20,20 +16,19 @@ def process_docker_image_dir(directory, client, registry):
     sub_directories = scandir(directory)
 
     for sub_dir in sub_directories:
-        print(sub_dir)
-        repository_parts = sub_dir.split(os.sep)[1:]
-        if len(repository_parts) >= 3:
+        if "Dockerfile" in os.listdir(sub_dir):
+            repository_parts = sub_dir.split(os.sep)[1:]
             repository_name = "/".join(repository_parts)
             repository_name = registry + "/master/" + repository_name
             build_path = os.path.abspath(sub_dir)
 
             print(f"Building image <{repository_name}> at path <{build_path}>")
-            # image, logs = client.images.build(path=build_path, tag=repository_name + ":latest")
-            # for item in logs:
-            #     print(item)
-            # # push the image to the registry
-            # for line in client.api.push(repository=repository_name, tag="latest", stream=True, decode=True):
-            #     print(line)
+            image, logs = client.images.build(path=build_path, tag=repository_name + ":latest")
+            for item in logs:
+                print(item)
+            # push the image to the registry
+            for line in client.api.push(repository=repository_name, tag="latest", stream=True, decode=True):
+                print(line)
 
 
 def scandir(start_dir) -> typing.List[typing.Tuple[str, str]]:
@@ -44,4 +39,5 @@ def scandir(start_dir) -> typing.List[typing.Tuple[str, str]]:
 
 
 if __name__ == '__main__':
+    load_dotenv(find_dotenv())
     build_images()
